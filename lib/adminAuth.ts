dcdd0a8000b9d7859dc2d2f38104c61bb878de00
연구개발전담부 <wdpb@mig.kr>
2026-01-19 13:30:14 +0900

    fix

--- lib/adminAuth.ts
+++ lib/adminAuth.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 
-export function assertAdmin() {
-  const c = cookies().get("admin")?.value;
+export async function assertAdmin() {
+  const c = (await cookies()).get("admin")?.value;
   if (c !== "1") {
     const err = new Error("Unauthorized");
     // @ts-ignore
